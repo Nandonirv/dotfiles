0a09104a9abd3ce10af985aeeb34dc1f13b104c1
Nandonirv <108136513+Nandonirv@users.noreply.github.com>
2025-05-18 17:17:49 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
 
-    nixosModules = import ./nixosModules/default.nix;
+    nixosModules = import ./nixosModules;
 
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#nixos'
