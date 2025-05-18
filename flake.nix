c9bd107c91ecbae0111648a9a4b2c2f08664d8e2
Nandonirv <matthewknowles@live.fr>
2025-05-18 15:16:29 +0200

    initial release

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Your new nix config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    # Supported systems for your flake packages, shell, etc.
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+
+    nixosModules = import ./nixosModules/nixos;
+
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#nixos'
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          ./nixosConfigurations/configuration.nix
+        ];
+      };
+    };
+}
