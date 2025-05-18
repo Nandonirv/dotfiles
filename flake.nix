0d81859078e389c99b0ab08a7be69aafb11facc5
Nandonirv <108136513+Nandonirv@users.noreply.github.com>
2025-05-18 15:41:30 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       nixos = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [
-          ./nixosConfigurations/default.nix
+          ./nixosConfigurations/default
         ];
       };
     };
